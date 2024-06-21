9ca09b8938ab973baa773bd052e26e4f98f2793b
Bruno-Elvis <brunoelvis@outlook..com.br>
2024-06-21 14:36:22 -0300

    Pequenos ajustes de UX para o mobile

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -63,7 +63,7 @@ export default function ContactForm({ buttonLabel }) {
 
   return (
     <>
-      <Form onSubmit={ handleSubmit }>
+      <Form onSubmit={ handleSubmit } noValidate>
         <FormGroup error={ getErrorMessageByFieldName({ fieldName: 'name'}) }>
           <Input
             placeholder='Nome'
@@ -74,9 +74,10 @@ export default function ContactForm({ buttonLabel }) {
 
         <FormGroup error={ getErrorMessageByFieldName({ fieldName: 'email'}) }>
           <Input
+            type='email'
             placeholder='E-mail'
             onChange={ handleEmailChange }
-            error={ getErrorMessageByFieldName({ fieldName: 'email'}) }/>
+            error={ getErrorMessageByFieldName({ fieldName: 'email'}) } />
 
         </FormGroup>
 
